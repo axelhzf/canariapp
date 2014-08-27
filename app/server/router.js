9aad355252156870decc35330e043062b983bf1b
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-08-27 18:42:35 +0100

    remove cache

--- app/server/router.js
+++ app/server/router.js
@@ -6,12 +6,6 @@ var router = new Router();
 var GoogleSpreadsheet = require("google-spreadsheet");
 var sheet = new GoogleSpreadsheet('1El5lOQrH-X9GgPRxWpMFw7AAI4i7DMDDYnBFQ82io08');
 
-var Cacheman = require("cacheman");
-var cache = new Cacheman("rows", {
-  ttl: "1m",
-  engine: 'memory'
-});
-
 router.get('/', function *() {
   var rows = yield getRows();
   var context = {
@@ -23,19 +17,11 @@ router.get('/', function *() {
 
 function getRows () {
   return new Promise(function (resolve, reject) {
-    cache.get("rows", function (err, rows) {
-      if (!err && rows) {
-        resolve(rows);
+    sheet.getRows("od6", function (err, row_data) {
+      if (err) {
+        reject(err);
       } else {
-        sheet.getRows("od6", function (err, row_data) {
-          if (err) {
-            reject(err);
-          } else {
-            cache.set("rows", row_data, function () {
-              resolve(row_data);
-            });
-          }
-        });
+        resolve(row_data);
       }
     });
   });
